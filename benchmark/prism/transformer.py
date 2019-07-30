7ae5587838e76b142aa09c4153a969cd2c21faa5
Ali ASADI <aasadi@krish-intern2.ist.local>
2019-07-30 11:55:48 +0200

    Add testcases for prism benchmark

--- benchmark/prism/transformer.py
+++ benchmark/prism/transformer.py
@@ -1,7 +1,13 @@
 import sys 
 
 input_file = sys.argv[1]
-output_file = 'out.txt'
+output_file = 'inp.txt'
+graph_file = 'graph.txt'
+
+choose_vertices = set()
+prob_vertices = set()
+choose_edges = []
+prob_edges = []
 
 with open(input_file) as inp:
     with open(output_file, 'w') as out:
@@ -11,10 +17,39 @@ with open(input_file) as inp:
             line = inp.readline().split(' ')[:-1]
             if len(line) == 5:
                 line = line[:-1]
-            line = line[:-1]  # ignore probability 
+        
+            prob = float(line[-1])
+            line = line[:-1] 
 
             from_, action, to = tuple(map(int, line))
             action_state = states + action * states + from_
 
             out.write('%s %s\n' % (from_, action_state))
             out.write('%s %s\n' % (action_state, to))
+
+            choose_vertices.add(from_)
+            prob_vertices.add(action_state)
+
+            choose_edges.append((from_, action_state))
+            prob_edges.append((action_state, to, prob))
+
+
+with open(graph_file, 'w') as out:
+    out.write("Choose Vertices:\n")
+    out.write("Number of Vertices: %s\n" % len(choose_vertices))
+    out.write(' '.join(map(str, choose_vertices)) + "\n")
+
+    out.write("Probability Vertices:\n")
+    out.write("Number of Vertices: %s\n" % len(prob_vertices))
+    out.write(' '.join(map(str, prob_vertices)) + "\n")
+
+    out.write("Choose Edges:\n")
+    out.write("Number of Edges: %s\n" % len(choose_edges))
+    for edge in choose_edges:
+        out.write("%s %s\n" % edge)
+
+    out.write("Probability Edges:\n")
+    out.write("Number of Edges: %s\n" % len(prob_edges))
+    for edge in prob_edges:
+        out.write("%s %s %s\n" % edge)
+
--- benchmark/prism/treewidth.py
+++ benchmark/prism/treewidth.py
@@ -1,21 +1,36 @@
 from collections import defaultdict
 
-input_file = 'out.txt'
+input_file = 'inp.txt'
+output_file = 'tree.txt'
 
 with open(input_file) as inp:
-    edges = ''.join(inp.readlines()).split('\n')[:-1]
-    graph_dict = defaultdict(set)
+    with open(output_file, 'w') as out:
+        edges = ''.join(inp.readlines()).split('\n')[:-1]
+        graph_dict = defaultdict(set)
 
-    for edge in edges:
-        v, u = edge.split(' ')
-        graph_dict[v].add(u)
-        graph_dict[u].add(v)
+        for edge in edges:
+            v, u = edge.split(' ')
+            graph_dict[v].add(u)
+            graph_dict[u].add(v)
 
-    for key in graph_dict.keys():
-        graph_dict[key] = list(graph_dict[key])
+        for key in graph_dict.keys():
+            graph_dict[key] = list(graph_dict[key])
 
-    print(len(graph_dict.keys()))
-    graph = Graph(graph_dict)
-    print graph.treewidth(k=20)
+        graph = Graph(graph_dict)
+        x = graph.treewidth(k=20, certificate=True)
+
+        vertices = list(x.vertices())
+        out.write("Vertices:\n")
+        out.write("Number of Vertices: %s\n" % len(vertices))
+        for i, vertex in enumerate(vertices):
+            out.write("%s\n" % ' '.join(vertex))
+
+        out.write("Edges:\n")
+        for fr, to, _ in x.edges():
+            fr_idx = vertices.index(fr)
+            to_idx = vertices.index(to)
+
+            out.write("%s %s\n" % (fr_idx, to_idx))
+        
 
 # run script with sagemath software
