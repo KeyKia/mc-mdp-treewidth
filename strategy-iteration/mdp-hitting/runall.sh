472ac421f7efc1157e0324f0cc1c82c3520a21d9
Amir KAFSHDAR GOHARSHADY <amir.goharshady@ist.ac.at>
2019-10-24 21:51:16 +0200

    Upload New File

---
+++ strategy-iteration/mdp-hitting/runall.sh
@@ -0,0 +1,20 @@
+g++ strategy-iteration.cpp -std=c++11 -osi
+for filename in ../../benchmarks/dacapobenchmark/outputs/raw_outputs/*.txt; do
+	name=${filename##*/};
+	name=${name%.txt};
+	echo "processing $name";
+	mkdir mcs-$name
+	python ../../mdp/mdp_utils/mdpprob_reformat.py ../../benchmarks/dacapobenchmark/outputs/raw_outputs/$name.txt mcs-$name
+	totaltime=0
+	for txtfile in mcs-$name/*.txt; do
+		echo textfile is $txtfile
+		./si $txtfile > $txtfile.log 
+		v=`cat $txtfile.log`
+		echo thistime is $v
+		echo $v + $totaltime | bc > temp.tmp
+		totaltime=`cat temp.tmp`
+		echo totaltime is $totaltime
+	done
+	echo $name $totaltime >> results.txt
+done
+
